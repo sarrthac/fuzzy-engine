993c57507aaac1c2730e3aabe358a500ab0a5693
Sarthak Zalte <sarthakzalte@gmail.com>
2024-09-17 21:54:14 +0530

    Added Admin creation api and YML file

--- src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
+++ src/main/java/net/engineeringdigest/journalApp/config/SpringSecurity.java
@@ -26,6 +26,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .antMatchers("/journal/**, /user/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and()
                 .httpBasic();
